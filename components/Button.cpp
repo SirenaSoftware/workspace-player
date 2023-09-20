56ea4a4fc4efaadb0d9f0570115c949c79372fc3
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2023-09-20 12:37:00 -0300

    Allow use of shared events

--- components/Button.cpp
+++ components/Button.cpp
@@ -15,39 +15,39 @@ Button::Button(QWidget *parent)
 void Button::onclick(bool checked){
     QString onclick = property("onclick").toString();
 
-    onclick = onclick.isEmpty()? this->objectName()+":onclick" : onclick;
+    onclick = onclick.isEmpty()? this->objectName()+".onclick" : onclick;
 
     if (L) {
-        luaL_dostring(L, (onclick+"("+(checked?"true":"false")+")").toStdString().c_str());
+        luaL_dostring(L, (onclick+"("+this->objectName()+(checked?",true":",false")+")").toStdString().c_str());
     }
 }
 
 void Button::onpress(){
     QString onpress = property("onpress").toString();
 
-    onpress = onpress.isEmpty()? this->objectName()+":onpress" : onpress;
+    onpress = onpress.isEmpty()? this->objectName()+".onpress" : onpress;
 
     if (L) {
-        luaL_dostring(L, (onpress+"()").toStdString().c_str());
+        luaL_dostring(L, (onpress+"("+this->objectName()+")").toStdString().c_str());
     }
 }
 
 void Button::onrelease(){
     QString onrelease = property("onrelease").toString();
 
-    onrelease = onrelease.isEmpty()? this->objectName()+":onrelease" : onrelease;
+    onrelease = onrelease.isEmpty()? this->objectName()+".onrelease" : onrelease;
 
     if (L) {
-        luaL_dostring(L, (onrelease+"()").toStdString().c_str());
+        luaL_dostring(L, (onrelease+"("+this->objectName()+")").toStdString().c_str());
     }
 }
 
 void Button::ontoggle(bool checked){
     QString onclick = property("ontoggle").toString();
 
-    onclick = onclick.isEmpty()? this->objectName()+":ontoggle" : onclick;
+    onclick = onclick.isEmpty()? this->objectName()+".ontoggle" : onclick;
 
     if (L) {
-        luaL_dostring(L, (onclick+"("+(checked?"true":"false")+")").toStdString().c_str());
+        luaL_dostring(L, (onclick+"("+this->objectName()+(checked?",true":"false")+")").toStdString().c_str());
     }
 }
